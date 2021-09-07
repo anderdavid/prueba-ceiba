140819bbff745b679d75e973c18d159beef31060
User <user.push@hackerrank.workspace>
2021-09-07 18:30:21 +0000

    Auto save changes
    Commit by HackerRank

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  console.log('hello');
   await app.listen(process.env.PORT || '8001');
 }
 bootstrap();
